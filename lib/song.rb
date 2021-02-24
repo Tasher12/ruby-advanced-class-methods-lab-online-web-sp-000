33435f582c4c04f54b0c5c41805fafbf6c9e4250
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-02-24 16:57:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,53 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song_name = self.new 
+    song_name.save 
+    song_name
+  end 
+  
 
+  def self.new_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new 
+    song.name = song_name
+    song.save  
+    song 
+  end
+  
+  def self.find_by_name(name)
+    self.all.find {|find| find.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end 
+  
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end 
+  
+  def self.new_from_filename(name)
+    self.all.each do |data|
+      data = row.split.gsub("-")
+      name = data[0]
+      artist_name = data[1].gsub(".mp3")
+      song = self.new 
+      song.name = name
+      song.artist_name = artist_name
+      song 
+   end 
+  end
+  
+  def self.destroy_all
+    @@all.clear 
+  end
+  
 end
